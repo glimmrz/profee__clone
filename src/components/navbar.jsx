fee0041965567163a0988ae56745db267cf93a65
tayef mahmud <mahmudovi183@gmail.com>
2024-09-21 14:15:46 +0600

    navbar style fix

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const pathname = usePathname();
-
+  console.log(pathname);
   return (
     <nav className="bg-transparent absolute w-full z-50">
       <Container>
@@ -21,7 +21,11 @@ export default function Navbar() {
               {/* Links */}
               <ul className="flex items-center gap-4">
                 <li
-                  className={`cursor-pointer text-white text-sm first-letter:capitalize hover:text-primary transition-colors duration-300 ${
+                  className={`cursor-pointer ${
+                    pathname === "/" || pathname.split("/")[1] === "send-money"
+                      ? "text-white"
+                      : "text-black"
+                  } text-sm first-letter:capitalize hover:text-primary transition-colors duration-300 ${
                     pathname.split("/")[1].replace(/-/, " ") === "send money"
                       ? "bg-mute rounded-md p-2 !text-black hover:!text-primary"
                       : ""
@@ -30,16 +34,24 @@ export default function Navbar() {
                   <Link href="/send-money">send money</Link>
                 </li>
                 <li
-                  className={`cursor-pointer text-white text-sm first-letter:capitalize hover:text-primary transition-colors duration-300 ${
+                  className={`cursor-pointer ${
+                    pathname === "/" || pathname.split("/")[1] === "send-money"
+                      ? "text-white"
+                      : "text-black"
+                  } text-sm first-letter:capitalize hover:text-primary transition-colors duration-300 ${
                     pathname.split("/")[1].replace(/-/, " ") === "media"
                       ? "bg-mute rounded-md p-2 !text-black hover:!text-primary"
                       : ""
                   }`}
                 >
-                  <Link href="#">media</Link>
+                  <Link href="media">media</Link>
                 </li>
                 <li
-                  className={`cursor-pointer text-white text-sm first-letter:capitalize hover:text-primary transition-colors duration-300 ${
+                  className={`cursor-pointer ${
+                    pathname === "/" || pathname.split("/")[1] === "send-money"
+                      ? "text-white"
+                      : "text-black"
+                  } text-sm first-letter:capitalize hover:text-primary transition-colors duration-300 ${
                     pathname.split("/")[1].replace(/-/, " ") === "blog"
                       ? "bg-mute rounded-md p-2 !text-black hover:!text-primary"
                       : ""
@@ -48,7 +60,11 @@ export default function Navbar() {
                   <Link href="#">blog</Link>
                 </li>
                 <li
-                  className={`cursor-pointer text-white text-sm first-letter:capitalize hover:text-primary transition-colors duration-300 ${
+                  className={`cursor-pointer ${
+                    pathname === "/" || pathname.split("/")[1] === "send-money"
+                      ? "text-white"
+                      : "text-black"
+                  } text-sm first-letter:capitalize hover:text-primary transition-colors duration-300 ${
                     pathname.split("/")[1].replace(/-/, " ") === "about us"
                       ? "bg-mute rounded-md p-2 !text-black hover:!text-primary"
                       : ""
